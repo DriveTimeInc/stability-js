a53fb3cd5d223e6d90a513024b1a064bd20cb0db
Curt Tyler <r.curt.tyler@gmail.com>
2017-05-26 12:19:07 -0700

    test: improve multi-failure circuit unit tests

--- tests/multi-failure-circuit-tests.js
+++ tests/multi-failure-circuit-tests.js
@@ -3,6 +3,14 @@ import Circuit from '../src/circuit';
 import MultiFailureCircuit from '../src/multi-failure-circuit';
 
 describe('MultiFailureCircuit', function () {
+	it('is enabled by default', function () {
+		// ARRANGE
+		const sut = new MultiFailureCircuit('test circuit', 0, 60000, 5);
+
+		// ASSERT
+		assert.equal(sut.state, Circuit.ENABLED);
+	});
+
 	it('does not move from enabled to disabled on the first error', function () {
 		// ARRANGE
 		const sut = new MultiFailureCircuit('test circuit', 0, 60000, 5);
@@ -14,7 +22,7 @@ describe('MultiFailureCircuit', function () {
 		assert.equal(sut.state, Circuit.ENABLED);
 	});
 
-	it('moves from enabled to disabled on the max number of errors', function () {
+	it('moves from enabled to disabled on the max number of consecutive errors', function () {
 		// ARRANGE
 		const sut = new MultiFailureCircuit('test circuit', 0, 60000, 5);
 
@@ -27,6 +35,22 @@ describe('MultiFailureCircuit', function () {
 
 		// ASSERT
 		assert.equal(sut.state, Circuit.DISABLED);
+	});
+
+	it('does not move from enabled to disabled on the max number of non-consecutive errors', function () {
+		// ARRANGE
+		const sut = new MultiFailureCircuit('test circuit', 0, 60000, 5);
+
+		// ACT
+		sut.emit('error', { msg: 'test error 1' });
+		sut.emit('error', { msg: 'test error 2' });
+		sut.emit('error', { msg: 'test error 3' });
+		sut.emit('success', { msg: 'test success' });
+		sut.emit('error', { msg: 'test error 4' });
+		sut.emit('error', { msg: 'test error 5' });
+
+		// ASSERT
+		assert.equal(sut.state, Circuit.ENABLED);
 	});
 
 	it('moves from disabled to half-open after cooldown', function (done) {
@@ -42,7 +66,6 @@ describe('MultiFailureCircuit', function () {
 		sut.emit('error', { msg: 'test error 5' });
 
 		// ASSERT
-		assert.equal(sut.state, Circuit.DISABLED);
 		setTimeout(() => {
 			assert.equal(sut.state, Circuit.HALFOPEN);
 			done();
@@ -64,14 +87,12 @@ describe('MultiFailureCircuit', function () {
 	it('does nothing on subsequent successes', function () {
 		// ARRANGE
 		const sut = new MultiFailureCircuit('test circuit', 0, 60000, 5);
-		assert.equal(sut.state, Circuit.ENABLED);
 		const expectedHistoryCount = sut.history.length;
 
 		// ACT
 		sut.emit('success', 0);
 
 		// ASSERT
-		assert.equal(sut.state, Circuit.ENABLED);
 		assert.equal(sut.history.length, expectedHistoryCount);
 	});
 });
